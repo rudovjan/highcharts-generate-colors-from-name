ed8c2c22226fb79a47f3a9bac809cc592c17c332
Jan Rudovsky <jrudovsky@vendavo.com>
2017-05-12 12:24:49 -0700

    Conditional allowing

--- demo/demo.js
+++ demo/demo.js
@@ -3,7 +3,10 @@ const series = [];
 for(let i = 0; i < 20; i++) {
   series[i] = {
     name: 'Series ' + i,
-    data: [3]
+    data: [3],
+    generateColor: {
+      enabled: true
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { generateColorForString } from './color.js'
 
 (function(H) {
-  H.wrap(H.Series.prototype, 'getColor', function (proceed) {
-
-    console.log(this.name);
-    const color = generateColorForString(this.name);
-    console.log(color);
-
-    this.color = color;
+  H.wrap(H.Series.prototype, 'getColor', function(proceed) {
+    if (this.options.generateColors && this.options.generateColors.enabled) {
+      this.color = generateColorForString(this.name);
+    } else {
+      // Execute original
+      proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+    }
   });
 }(Highcharts));
